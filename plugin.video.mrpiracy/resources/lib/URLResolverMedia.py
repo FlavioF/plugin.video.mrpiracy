b01e9fc07502c260927cfffbb32b7b68ff1ddd17
Alberto <alberto.csm@gmail.com>
2015-12-18 01:43:13 +0000

    dont blow up if no subs available

--- plugin.video.mrpiracy/resources/lib/URLResolverMedia.py
+++ plugin.video.mrpiracy/resources/lib/URLResolverMedia.py
@@ -159,9 +159,13 @@ class Vidzi():
 			for pack in re.finditer('(eval\(function.*?)</script>', sourceCode, re.DOTALL):
 				dataJs = jsunpacker.unpack(pack.group(1)) # Unpacker for Dean Edward's p.a.c.k.e.r | THKS
 				stream = re.search('file\s*:\s*"([^"]+)', dataJs)
-				subtitle = re.compile('tracks:\[\{file:"(.+?)\.srt"').findall(dataJs)[0]
-				subtitle += ".srt"
-				self.subtitle = subtitle
+				try:
+					subtitle = re.compile('tracks:\[\{file:"(.+?)\.srt"').findall(dataJs)[0]
+					subtitle += ".srt"
+					self.subtitle = subtitle
+				except:
+					pass
+
 				if stream:
 					return stream.group(1)
 
